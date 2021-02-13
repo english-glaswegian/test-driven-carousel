ad7284b617ea40d795e4581e23f4a8ca9eb9e76c
John Pitts <irvinenomore@gmail.com>
2021-02-13 11:34:28 -0500

    :construction: Staring work on CarouselButton

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,23 @@
 // .eslintrc.js
 module.exports = {
-    extends:['eslint:recommended'],
-    parserOptions: {
-        ecmaVersion: 6,
-        sourceType: 'module',
-        ecmaFeatures: {
-            jsx: true,
-        },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
-    env: {
-        node: true,
+  },
+  env: {
+    node: true,
+  },
+  rules: {
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'comma-dangle': ['error', 'always-multiline'],
+  },
+  settings: {
+    react: {
+      version: '16.4.2',
     },
-    rules: {
-        quotes: ['error', 'single', { avoidEscape: true}],
-        'comma-dangle': ['error', 'always-multiline'],
-    },
-    settings: {
-        react: {
-            version: '17.0.1',
-        },
-    },
+  },
 };
