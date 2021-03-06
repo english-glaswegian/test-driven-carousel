bbb54b79119424c7d4a26850599f62bf53414317
John Pitts <irvinenomore@gmail.com>
2021-03-06 16:31:08 -0500

    :white_check_mark: Update tests for styled components

--- src/tests/CarouselSlide.test.js
+++ src/tests/CarouselSlide.test.js
@@ -1,49 +1,65 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import CarouselSlide from '../CarouselSlide';
 
 describe('CarouselSlide', () => {
-    let wrapper;
+  let wrapper;
 
-    beforeEach(() => {
-        wrapper = shallow(
-            <CarouselSlide
-                imgUrl='https://example.com/default.jpg'
-                description='Default test image' />);
-    });
-    it('renders a <figure>', () => {
-        expect(wrapper.type()).toBe('figure');
-    });
+  beforeEach(() => {
+    wrapper = shallow(
+      <CarouselSlide
+        imgUrl='https://example.com/default.jpg'
+        description='Default test image'
+      />
+    );
+  });
+  it('renders a <figure>', () => {
+    expect(wrapper.type()).toBe('figure');
+  });
 
-    it('renders an <img> and a <figcaption> as children', () => {
-        expect(wrapper.childAt(0).type()).toBe('img');
-        expect(wrapper.childAt(1).type()).toBe('figcaption');
-    })
-    
-    it('passes `imgUrl` through to the <img>', () => {
-        const imgUrl = 'https://example.com/image.png';
-        wrapper.setProps({ imgUrl });
-        const img = wrapper.find('img');
-        expect(img.prop('src')).toBe(imgUrl);
-    })
-    
-    it('uses `description` and `attribution` as the <figcaption>', () => {
-        const description = 'A jaw-droppingly spectacular image';
-        const attribution = 'Trevor Burnham';
-        wrapper.setProps({ description, attribution });
-        expect(wrapper.find('figcaption').text()).toBe(`${description} ${attribution}`)
-        expect(wrapper.find('figcaption strong').text()).toBe(description);
-    });
-    
-    it('passes other props through to the <figure>', () => {
-        const style = {};
-        const onClick = () => { };
-        const className = 'my-carousel-slide';
-        wrapper.setProps({ style, onClick, className });
-        expect(wrapper.prop('style')).toBe(style);
-        expect(wrapper.prop('onClick')).toBe(onClick);
-        expect(wrapper.prop('className')).toBe(className);
-    })
-    
+  it('renders an props.Img and a <figcaption> as children', () => {
+    expect(wrapper.childAt(0).type()).toBe(CarouselSlide.defaultProps.Img);
+    expect(wrapper.childAt(1).type()).toBe('figcaption');
+  });
+
+  it('passes `imgUrl` through to props.Img', () => {
+    const imgUrl = 'https://example.com/image.png';
+    wrapper.setProps({ imgUrl });
+    const img = wrapper.find(CarouselSlide.defaultProps.Img);
+    expect(img.prop('src')).toBe(imgUrl);
+  });
+
+  it('uses `description` and `attribution` as the <figcaption>', () => {
+    const description = 'A jaw-droppingly spectacular image';
+    const attribution = 'Trevor Burnham';
+    wrapper.setProps({ description, attribution });
+    expect(wrapper.find('figcaption').text()).toBe(
+      `${description} ${attribution}`
+    );
+    expect(wrapper.find('figcaption strong').text()).toBe(description);
+  });
+
+  it('passes other props through to the <figure>', () => {
+    const style = {};
+    const onClick = () => {};
+    const className = 'my-carousel-slide';
+    wrapper.setProps({ style, onClick, className });
+    expect(wrapper.prop('style')).toBe(style);
+    expect(wrapper.prop('onClick')).toBe(onClick);
+    expect(wrapper.prop('className')).toBe(className);
+  });
 });
 
+describe('Img', () => {
+  let mounted;
+  const imgUrl = 'https://example.com/default.jpg';
+
+  beforeEach(() => {
+    const Img = CarouselSlide.defaultProps.Img;
+    mounted = mount(<Img src={imgUrl} imgHeight={500} />);
+  });
+
+  it('renders an <img> with the given src', () => {
+    expect(mounted.containsMatchingElement(<img src={imgUrl} />)).toBe(true);
+  });
+});
