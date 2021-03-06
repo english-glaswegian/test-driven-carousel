c56fa7833420c6c423b884c32adfbc02aa35f386
John Pitts <irvinenomore@gmail.com>
2021-03-06 12:06:04 -0500

    :wrench: Initial stylelint setup

--- .babelrc.js
+++ .babelrc.js
@@ -1,5 +1,8 @@
 // .babelrc.js
 module.exports = {
-    presets: ['@babel/preset-react', '@babel/preset-env'],
-    plugins: ['@babel/plugin-proposal-class-properties'],
+  presets: ['@babel/preset-react', '@babel/preset-env'],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    'babel-plugin-styled-components',
+  ],
 };
